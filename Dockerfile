e251b1442bb5044d385bd3f0e75c4d9b7696fc17
Wuifi <geyerwo@gmx.de>
2021-05-06 15:50:38 +0100

    update dockerfile and python script

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,15 @@
-FROM python:3.8-slim
+FROM arm32v7/python:3.8-slim-buster
+
+#FROM python:3.8-slim-buster
+
+ENV VIRTUAL_ENV=/opt/venv
+RUN python3 -m venv $VIRTUAL_ENV
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
 
 COPY requirements.txt /
 COPY kostal2mqtt.py /
+#RUN pip3 install git+https://github.com/rcasula/kostalpiko.git
+RUN pip3 install --no-cache-dir -r requirements.txt
+CMD [ "python3", "./kostal2mqtt.py" ]
 
-RUN pip install --no-cache-dir -r requirements.txt
-CMD [ "python", "./kostal2mqtt.py" ]
+
--- kostal2mqtt.py
+++ kostal2mqtt.py
@@ -11,14 +11,19 @@ import paho.mqtt.client as mqtt
 mqttc = mqtt.Client()
 import paho.mqtt.publish as publish
 
+kostal_username='pvserver' # os.environ.get('KOSTAL_USERNAME')
+kostal_password='mcguire49' # os.environ.get('KOSTAL_PASSWORD')
+kostal_host='http://192.168.178.110' # os.environ.get('KOSTAL_HOST')
 ## User Config from Environmental variables:
-kostal_host='http://'+ os.environ.get('KOSTAL_HOST')
-kostal_username= os.environ.get('KOSTAL_USERNAME')
-kostal_password= os.environ.get('KOSTAL_PASSWORD')
-mqtt_topic= os.environ.get('MQTT_TOPIC')
-mqtt_host= os.environ.get('MQTT_HOST')
-mqtt_username= os.environ.get('MQTT_USERNAME')
-mqtt_password= os.environ.get('MQTT_PASSWORD')
+#kostal_host='http://'+ os.environ.get('KOSTAL_HOST')
+#kostal_username= os.environ.get('KOSTAL_USERNAME')
+#kostal_password= os.environ.get('KOSTAL_PASSWORD')
+mqtt_topic="KOSTAL" # os.environ.get('MQTT_TOPIC')
+mqtt_host="192.168.177.100" # os.environ.get('MQTT_TOPIC')
+#mqtt_topic= os.environ.get('MQTT_TOPIC')
+#mqtt_host= os.environ.get('MQTT_HOST')
+#mqtt_username= os.environ.get('MQTT_USERNAME')
+#mqtt_password= os.environ.get('MQTT_PASSWORD')
 
 ## Internal variables
 mqtt_port=1883 # os.environ.get('MQTT_PORT')
@@ -33,12 +38,23 @@ scrape_interval=10 # os.environ.get('SCRAPE_INTERVAL')
 
 
 ## get data
-#create a new piko instance
-#p = Piko('host', 'username', 'password')
-p = Piko(kostal_host,kostal_username,kostal_password)
+def getpikodata(kostal_host,kostal_username,kostal_password):
+    #create a new piko instance
+    #p = Piko('host', 'username', 'password')
+    p = Piko(kostal_host,kostal_username,kostal_password)
+    try:
+        raw_content=p._get_raw_content()
+        connection_ok=True
+    except (ValueError, TypeError) as ex:
+        #debug_str='"%s" cannot be converted to an float: %s' % (raw_str, ex)
+        print(ex)
+        connection_ok=False
+        raw_content=[]
+
+    return raw_content, connection_ok
 
 ## convert data 2 json
-def raw2json(raw):
+def raw2json_old(raw):
     #['1076', '96603', '17.77', '583', '229', '0.94', '356', '0', '231', '0.00', '363', '596', '228', '0.95', '357', 'Einspeisen MPP']
     if raw!=None:
         if type(raw[1]) is str:
@@ -91,7 +107,6 @@ def raw2json(raw):
         state_string='No data'
         state_binary=None
         state_flag=-1
-        
     json_string = {"PIKO":{"ENERGY":{"actual":act,"total":total,"daily":daily},
                            "IN":{"STRING1":{"voltage":string1_voltage,"current":string1_current},
                                  "STRING2":{"voltage":string2_voltage,"current":string2_current},
@@ -99,8 +114,92 @@ def raw2json(raw):
                            "OUT":{"PHASE1":{"voltage":phase1_voltage,"power":phase1_power},
                                   "PHASE2":{"voltage":phase2_voltage,"power":phase2_power},
                                   "PHASE3":{"voltage":phase3_voltage,"power":phase3_power}},
-                           "STATE":{"string":state_string,"binary":state_binary,"flag":state_flag}}}
+                           "STATE":{"string":state_string,"binary":state_binary,"flag":state_flag,"debug":debug_str}}}
     return json_string
+
+    ## convert data to json
+def raw2json(raw):
+    act=None
+    total=None
+    daily=None
+    string1_voltage=None
+    phase1_voltage=None
+    string1_current=None
+    phase1_power=None
+    string2_voltage=None
+    phase2_voltage=None
+    string2_current=None
+    phase2_power=None
+    string3_voltage=None
+    phase3_voltage=None
+    string3_current=None
+    phase3_power=None
+    state_string='No data'
+    state_binary=None
+    state_flag=-1
+    try:
+        act=float(raw[0])
+        total=float(raw[1])
+        daily=float(raw[2])
+        string1_voltage=float(raw[3])
+        phase1_voltage=float(raw[4])
+        string1_current=float(raw[5])
+        phase1_power=float(raw[6])
+        string2_voltage=float(raw[7])
+        phase2_voltage=float(raw[8])
+        string2_current=float(raw[9])
+        phase2_power=float(raw[10])
+        string3_voltage=float(raw[11])
+        phase3_voltage=float(raw[12])
+        string3_current=float(raw[13])
+        phase3_power=float(raw[14])
+        state_string=raw[15] # last value
+        if "Aus" in state_string:
+            state_binary=False,
+            state_flag=1
+        elif "Leerlauf" in state_string:
+            state_binary=False,
+            state_flag=2    
+        elif "Einspeisen" in state_string:
+            state_binary=True
+            state_flag=3
+        else: 
+            state_binary=None,
+            state_flag=0
+        debug_str="OK"
+        state_flag=1    
+    except (ValueError, TypeError) as ex:
+        act=None
+        total=None
+        daily=None
+        string1_voltage=None
+        phase1_voltage=None
+        string1_current=None
+        phase1_power=None
+        string2_voltage=None
+        phase2_voltage=None
+        string2_current=None
+        phase2_power=None
+        string3_voltage=None
+        phase3_voltage=None
+        string3_current=None
+        phase3_power=None
+        state_string='No data'
+        state_binary=None
+        state_flag=-1
+        debug_str='Received Data: "%s" LOG: %s' % (raw_content, ex)
+        #print(debug_str)
+
+    json_string = {"PIKO":{"ENERGY":{"actual":act,"total":total,"daily":daily},
+                           "IN":{"STRING1":{"voltage":string1_voltage,"current":string1_current},
+                                 "STRING2":{"voltage":string2_voltage,"current":string2_current},
+                                 "STRING3":{"voltage":string3_voltage,"current":string3_current}},
+                           "OUT":{"PHASE1":{"voltage":phase1_voltage,"power":phase1_power},
+                                  "PHASE2":{"voltage":phase2_voltage,"power":phase2_power},
+                                  "PHASE3":{"voltage":phase3_voltage,"power":phase3_power}},
+                           "STATE":{"string":state_string,"binary":state_binary,"flag":state_flag,"debug":debug_str}}}
+    return json_string, state_flag 
+
 ## publish data 2 mqtt
 def convert_to_mqtt_msg(topic,dict_input):
     json_object = json.dumps(dict_input, indent = 4) 
@@ -113,14 +212,33 @@ def convert_to_mqtt_msg(topic,dict_input):
     #print(type(msg))
     return msg
 
+def publish2mqtt(MQTT_msg):
+    #print(MQTT_msg)   
+    msgs = [MQTT_msg]
+    # print(msgs)
+    publish.multiple(
+        msgs, hostname=mqtt_host,
+        port=mqtt_port,
+        client_id=mqtt_client_id,
+        keepalive=mqtt_keepalive,
+        will=mqtt_will,
+        auth=mqtt_auth,
+        tls=mqtt_tls,
+        protocol=mqtt.MQTTv311,
+        transport="tcp"
+        )
+    return 
 ## Program
+print('kostal2mqtt.py started: \n Trying to connect to: \n',kostal_host,'-------------------------------')
 
 while True:
-    raw_content=p._get_raw_content()
-    jsonData=raw2json(raw_content)
-    MQTT_msg=convert_to_mqtt_msg(mqtt_topic,jsonData)
-    msgs = [MQTT_msg]
-    publish.multiple(msgs, hostname=mqtt_host,
-                     port=mqtt_port, client_id=mqtt_client_id, keepalive=mqtt_keepalive, will=mqtt_will, auth=mqtt_auth, tls=mqtt_tls,
-                     protocol=mqtt.MQTTv311, transport="tcp")
+    try:
+        raw_content, connection_ok=getpikodata(kostal_host,kostal_username,kostal_password)
+        json_string, state_flag=raw2json(raw_content)
+        print(json_string)
+        MQTT_msg=convert_to_mqtt_msg(mqtt_topic,json_string)
+        publish2mqtt(MQTT_msg)
+    except (ValueError, TypeError) as ex:
+        #debug_str='"%s" cannot be converted to an float: %s' % (raw_content, ex)
+        print(ex)
     time.sleep(scrape_interval)
